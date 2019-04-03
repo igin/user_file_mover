77371cd77ac1085333a6ba9aa07fb2d5949b13f0
Nicholas Pleschko <n.pleschko@gmail.com>
2019-04-03 20:48:54 +0200

    fix DS_Store problem

--- reorganize_export.py
+++ reorganize_export.py
@@ -2,19 +2,23 @@ import os
 import shutil
 import sys
 
-def move_user_files_to_combined_directory(input_dir, output_dir, file_prefix=None):
+def move_user_files_to_combined_directory(input_directory, output_dir, file_prefix=None):
     if file_prefix is None:
-        file_prefix = os.path.basename(input_dir)
+        file_prefix = os.path.basename(input_directory)
 
-    for user_id in os.listdir(input_dir):
+    for user_id in os.listdir(input_directory):
+        if user_id[0] == '.':
+            continue
         output_user_dir = os.path.join(output_dir, user_id)
         try:
             os.makedirs(output_user_dir)
         except Exception as e:
             pass
 
-        input_user_dir = os.path.join(input_dir, user_id)
+        input_user_dir = os.path.join(input_directory, user_id)
         for filename in os.listdir(input_user_dir):
+            if filename[0] == '.':
+                continue
             new_filename = user_id + '_' + file_prefix + '_' + filename
             shutil.copyfile(
                 os.path.join(input_user_dir, filename),
@@ -27,3 +31,5 @@ if __name__ == '__main__':
 
     for input_dir in input_dirs:
         move_user_files_to_combined_directory(input_dir, './user_daten')
+
+    print("done")
